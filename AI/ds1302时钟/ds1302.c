1f5b7937eb40c32ae7f01375bb5281788acda9ef
kailangjiao <58363082+kailangjiao@users.noreply.github.com>
2019-12-07 10:54:54 +0800

    Add files via upload

---
+++ AI/ds1302时钟/ds1302.c
@@ -0,0 +1,6 @@
+#include"ds1302.h"
+				   
+void ds1302writebyte(u8 dat)
+{
+	
+}
---
+++ AI/ds1302时钟/ds1302.h
@@ -0,0 +1,18 @@
+#ifndef_ds1302_H_
+#define_ds1302_H_
+
+#include"reg51.h"
+sbit sclk=P3^6;
+sbit inpoint=P3^4;
+sbit rst=P3^5;
+
+#ifndef u8
+#define	u8 unsinged char
+#endif
+
+#ifndef u16
+#define u16 unsinged int
+#endif
+
+
+#endif
